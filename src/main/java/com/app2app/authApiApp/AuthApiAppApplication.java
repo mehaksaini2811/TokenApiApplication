61104cec7ab274faff2b02b98be48468e1592857
Mehak Saini <mehak@192.168.1.9>
2022-04-19 10:29:21 +0530

    My first commit

---
+++ src/main/java/com/app2app/authApiApp/AuthApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.app2app.authApiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app2app/authApiApp/AuthApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app2app.authApiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
